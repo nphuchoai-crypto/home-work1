a1ba232bfc7827e29c499ef8ef1af7be040fe94c
nphuchoai-crypto <nphuchoai@gmail.com>
2026-07-13 13:25:35 +0900

    uploard source code

---
+++ homework.py
@@ -0,0 +1,15 @@
+
+price = 500000
+discount_percent = 0.1
+khuyen_mai = price * discount_percent
+print("Khuyến mãi:", str(khuyen_mai) + " VND")
+thanh_tien = price - khuyen_mai
+print("Thành tiền:", str(thanh_tien) + " VND")
+salary_per_day = 300000
+working_days = 22
+total_salary = salary_per_day * working_days
+print("Tổng lương:", str(total_salary) + " VND")
+distance_km = 12
+cost_per_km = 5000
+total_cost = distance_km * cost_per_km
+print("Tổng chi phí:", str(total_cost) + " VND")
